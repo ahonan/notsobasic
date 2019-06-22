a23d2a8d61a88c9b707b13c30c65e4da2bb7bfc5
Aidan <ajhonan123@gmail.com>
2019-06-22 07:10:40 +0000

    add much design and some js

--- site.js
+++ site.js
@@ -1,5 +1,22 @@
 /*global $*/
 var main=function(){
-  $(window).resize(function(){});
+  //setup
+  
+//   $('.text_box').each(function(i,obj) {});
+  
+  //click fns
+  $('#minus').click(function() {
+      if($('.option').length > 2) {
+          $('#joshua :last-child').remove();
+      }
+  });
+  
+  $('#plus').click(function() {
+      $('#joshua').append('<li class="option"><input class="text_box" placeholder="Fill me in please"><span class="underliner"></span></li>');
+  });
+  
+  $('#decide').click(function() {
+      
+  });
 };
 $(document).ready(main);
