ed47a884c66e2de80cfb0039f462e07d8e2f5282
Aidan Honan <ahonan@jbssinc.com>
2019-07-03 13:41:27 -0500

    add second decide bounce (maybe lol)

--- site.js
+++ site.js
@@ -2,18 +2,18 @@
 var main=function(){
   //setup
   var decision_open = false;
-  
+
   //click fns
   $('#minus').click(function() {
       if($('.option').length > 2) {
           $('#joshua li:last-child').remove();
       }
   });
-  
+
   $('#plus').click(function() {
       $('#joshua').append('<li class="option"><input class="text_box" placeholder="Fill me in please"><span class="underliner"></span></li>');
   });
-  
+
   $('#decide').click(function() {
     var opts = [];
     $('#joshua .text_box').each(function(i,obj) {
@@ -26,9 +26,12 @@ var main=function(){
         $('#hidden_break').css('display','block');
         $('#decision_container').css({height:"60px",transition:"height .5s cubic-bezier(.17,.89,.61,1.3)"});
       } else {
-        $('#decision_container').animate({height:"120px"},1000,"linear");
+        $('#decision_container').css({height:"0px"});
+        setTimeout(function(){
+          $('#decision_container').css({height:"60px"});
+        }, 500);
       }
-      
+
       decision_open = true;
     }else {
       $('#decision_container').css({height:0,transition:"height .5s cubic-bezier(.17,.89,.61,1.3)"});
