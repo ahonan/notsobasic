83933649129ee6486abc1182480cc7910c6c6bae
Aidan Honan <ahonan@jbssinc.com>
2019-07-03 14:08:26 -0500

    fix stupid timer before 2nd hit out transition

--- site.js
+++ site.js
@@ -29,7 +29,7 @@ var main=function(){
         $('#decision_container').css({height:"0px",transition:"height 0s"});
         setTimeout(function(){
           $('#decision_container').css({height:"60px",transition:"height .5s cubic-bezier(.17,.89,.61,1.3)"});
-        }, 500);
+        }, 1);
       }
 
       decision_open = true;
